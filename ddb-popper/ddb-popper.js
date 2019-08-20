0920a87132fc8b78b669e94db715bc2b4f247a7c
eclarke12 <therealevan@gmail.com>
2019-08-19 22:08:38 -0700

    basic logic

--- ddb-popper/ddb-popper.js
+++ ddb-popper/ddb-popper.js
@@ -1,17 +1,28 @@
 ( () => {
-    Hooks.on('renderActorSheet5eCharacter', (app, html, data) => {
+    Hooks.on("renderActorSheet5eCharacter", (app, html, data) => {
+	        const windowHeader = html.parent().parent().find(".window-header");
+            const windowCloseBtn = windowHeader.find(".close");
+            const ddbLogo = "https://www.dndbeyond.com/Content/1-0-514-0/Skins/Waterdeep/images/dnd-beyond-b-red.png"
+            const imgStyle = `display:inline-block;vertical-align:middle;height:16px`;
+            //const imgStyle = `height:16px`;
+            const aStyle = `display:inline-block;vertical-align:baseline`;
+            //const aStyle = ""
+            const ddbButton = $(`<a class="ddb-popup" style=${aStyle}><img src=${ddbLogo} style=${imgStyle}> DDB</a>`);
+            let ddbUrl = "https://www.dndbeyond.com/profile/errational/characters/15260756"
+            
+            windowHeader.find('.ddb-popup').remove();
+            windowCloseBtn.before(ddbButton);
 
-	    const windowHeader = html.parent().parent().find('.window-header');
-        const windowCloseBtn = windowHeader.find('.close');
-        const importButton = $('<a class="ddb-popup"><img src="https://www.dndbeyond.com/Content/1-0-514-0/Skins/Waterdeep/images/dnd-beyond-b-red.png" height="15px"/> DDB</a>');
+            // Handle button clicks
+            ddbButton.click(ev => {
+                ev.preventDefault();
+                let existingPopup = window.open("", "ddb-popup", "resizeable,scrollbars,location=no,width=768,height=1024");
 
-        windowHeader.find('.ddb-popup').remove();
-         windowCloseBtn.before(importButton);
-
-        // Handle button clicks
-        importButton.click(ev => {
-            ev.preventDefault();
-            window.open("https://www.dndbeyond.com/profile/errational/characters/15260756","ddb-popup","resizeable,scrollbars")
+                if(existingPopup.location.href == "about:blank"){
+                    existingPopup.location = ddbUrl;
+                } else {
+                    window.open("","ddb-popup",);
+                }
+            });
         });
-    });
 })();
