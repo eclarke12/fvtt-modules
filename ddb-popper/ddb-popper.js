d52924a8601d29e45f46ca94ec101329ff1e79bd
eclarke12 <therealevan@gmail.com>
2019-08-20 18:07:05 -0700

    fix formapp

--- ddb-popper/ddb-popper.js
+++ ddb-popper/ddb-popper.js
@@ -1,20 +1,22 @@
 Hooks.on("ready", () => {
-    ddbPopper = new DDBPopper();
+    let ddbPopper = new DDBPopper();
 });
 
 class DDBPopper {
     constructor(){
-
+        this._registerSettings();
+        this._hookRenderActorSheet();
     }
 
     CONFIG = {
-        ddbLogo: "https://www.dndbeyond.com/Content/1-0-514-0/Skins/Waterdeep/images/dnd-beyond-b-red.png",
+        ddbLogo: "https://www.dndbeyond.com/Content/1-0-518-0/Skins/Waterdeep/images/dnd-beyond-b-red.png",
         imgStyle: `display:inline-block;vertical-align:middle;height:16px`,
         aStyle: `display:inline-block;vertical-align:baseline`
     }
 
     _hookRenderActorSheet() {
         Hooks.on("renderActorSheet5eCharacter", (app, html, data) => {
+            console.log("app:",app,"html:",html,"data:",data);
             this._addDDBButton(html, data)
         });
     }
@@ -42,20 +44,77 @@ class DDBPopper {
 
         ddbButton.contextmenu(ev => {
             ev.preventDefault();
-            ddbURLForm = new DDBURLEntryForm(data, {closeOnSubmit: true});
-            ddbURLForm.render();
+            new DDBURLEntryForm(data, {closeOnSubmit: true}).render(true);
         });
     }
 
     async _updateActor (actor, data) {
-        await actor.update(data);
+        const flags = {
+            "flags": {
+                "ddb-popper.ddbURL": data
+            }
+        }
+        await actor.update(flags,{displaySheet: false});
     }
 }
     
 class DDBURLEntryForm extends FormApplication {
-    constructor(object, options){
-        super.constructor(object, options);
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            id: "ddb-url",
+            title: "D&D Beyond URL",
+            template: "public/modules/ddb-popper/template/ddb-popper.html",
+            classes: ["sheet"],
+            width: 500
+        });
     }
 
+    /**
+     * for a given actor (by id), find the setting
+     */
+    async getData() {
+        return {
+            settings: game.settings.get("ddb-popper", data.id)
+        };
+    }
+
+    _updateObject(event, formData) {
+        try {
+            game.settings.set("ddb-popper", data.id, formdata.ddbURL);
+        } catch (e) {
+            if(e.message == "That is not a registered game setting") {
+                game.settings.register("ddb-popper", data.id, formdata.ddbURL);
+            }
+        }
+        
+    }
 
 }
+
+class CombatTrackerConfig extends FormApplication {
+    static get defaultOptions() {
+      const options = super.defaultOptions;
+      options.id = "combat-config";
+      options.title = "Combat Tracker Configuration";
+      options.classes = ["sheet", "combat-sheet"];
+      options.template = "templates/sheets/combat-config.html";
+      return options;
+    }
+  
+    /* -------------------------------------------- */
+  
+    async getData() {
+      const data = {
+        settings: game.settings.get("core", Combat.CONFIG_SETTING)
+      };
+      return data;
+    };
+  
+    /* -------------------------------------------- */
+  
+    _updateObject(event, formData) {
+      game.settings.set("core", Combat.CONFIG_SETTING, {
+        resource: formData.resource
+      });
+    }
+  }
